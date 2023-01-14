fe78762ca643ab5da1da96fac2275e81f9752d5b
artisan.jenkins <artisan.jenkins@hgs.hiddengeniusproject.org>
2023-01-14 15:24:27 -0500

    finsihed madlib

---
+++ Fun_of_Python/Malibs/READme.md
@@ -0,0 +1,12 @@
+Madlibs: Key Concepts
+
+Output - madlibs uses the function print() to communicate in terminal
+String (data Type) - letters and text 
+string literal - an oblect containing text
+String Concatenation (different ways) - different ways to output a string
+
+fString - allows the user to use a string as normal and add to curly brackets to pull data from other variables
+
+variables - something that holds data 
+input - allows user to input data as program runs 
+Comments - they make good for notes
---
+++ Fun_of_Python/Malibs/madlibs.py
@@ -0,0 +1,18 @@
+# string concetentions (how to putt strings together)
+# suppose we watn to create a string that says "subscribe to ____"
+#youtuber = "Mr. Pang"
+
+# a few ways to do so 
+#print("subscribe to " + youtuber)
+#print("subscribe to {}".format(youtuber))
+#print(f"subscribe to {youtuber}")
+
+adj = input("Adjective: ")
+verb1 = input("Verb: ")
+verb2 = input ("Another one: ")
+famous_person = input("Famous Person: ")
+
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
+    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+
+print(madlib)
