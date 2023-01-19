409f949c13a57d9944816e71c7976e47317682f9
artisan.jenkins <artisan.jenkins@hgs.hiddengeniusproject.org>
2023-01-18 21:21:20 -0500

    update

--- Fun_of_Python/Malibs/madlibs.py
+++ Fun_of_Python/Malibs/madlibs.py
@@ -12,7 +12,6 @@ verb1 = input("Verb: ")
 verb2 = input ("Another one: ")
 famous_person = input("Famous Person: ")
 
-madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \
-    I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
+madlib = f"Computer programming is so {adj}! It makes me so excited all the time because \I love to {verb1}. Stay hydrated and {verb2} like you are {famous_person}!"
 
 print(madlib)
